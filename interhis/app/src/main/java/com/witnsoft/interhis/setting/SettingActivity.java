7d805f85ae1aec79ac8d2491b6feb06edf2e0039
郑程鹏 <zhengchengpengisme@gmail.com>
2017-06-13 18:48:38 +0800

    增加：设置右侧fragment

--- interhis/app/src/main/java/com/witnsoft/interhis/setting/SettingActivity.java
+++ interhis/app/src/main/java/com/witnsoft/interhis/setting/SettingActivity.java
@@ -1,14 +1,27 @@
 package com.witnsoft.interhis.setting;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
+import com.jakewharton.rxbinding.view.RxView;
 import com.witnsoft.interhis.R;
+import com.witnsoft.interhis.setting.about.AboutFragment;
+import com.witnsoft.interhis.setting.myhistory.MyHistoryFragment;
+import com.witnsoft.interhis.setting.myincome.MyIncomeFragment;
+import com.witnsoft.interhis.setting.myinfo.MyInfoFragment;
 import com.witnsoft.libinterhis.base.BaseActivity;
 
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
+
+import java.util.concurrent.TimeUnit;
+
+import rx.functions.Action1;
 
 /**
  * Created by zhengchengpeng on 2017/6/13.
@@ -17,6 +30,8 @@ import org.xutils.x;
 @ContentView(R.layout.activity_setting)
 public class SettingActivity extends BaseActivity {
 
+    @ViewInject(R.id.ll_back)
+    private LinearLayout llBack;
     // 我的信息
     @ViewInject(R.id.rl_my_info)
     private RelativeLayout rlMyInfo;
@@ -30,19 +45,85 @@ public class SettingActivity extends BaseActivity {
     @ViewInject(R.id.rl_about)
     private RelativeLayout rlAbout;
 
+    private FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         x.view().inject(this);
         initCLick();
+        fragmentManager = getFragmentManager();
         init();
     }
 
     private void initCLick() {
-
+        // 返回
+        RxView.clicks(llBack)
+                .throttleFirst(500, TimeUnit.MILLISECONDS)
+                .compose(this.<Void>bindToLifecycle())
+                .subscribe(new Action1<Void>() {
+                    @Override
+                    public void call(Void aVoid) {
+                        finish();
+                    }
+                });
+        // 我的信息
+        RxView.clicks(rlMyInfo)
+                .throttleFirst(500, TimeUnit.MILLISECONDS)
+                .compose(this.<Void>bindToLifecycle())
+                .subscribe(new Action1<Void>() {
+                    @Override
+                    public void call(Void aVoid) {
+                        MyInfoFragment myInfoFragment = new MyInfoFragment();
+                        setChecked(myInfoFragment, rlMyInfo, rlAbout, rlMyHistory, rlMyIncome);
+                    }
+                });
+        // 我的问诊记录
+        RxView.clicks(rlMyHistory)
+                .throttleFirst(500, TimeUnit.MILLISECONDS)
+                .compose(this.<Void>bindToLifecycle())
+                .subscribe(new Action1<Void>() {
+                    @Override
+                    public void call(Void aVoid) {
+                        MyHistoryFragment myHistoryFragment = new MyHistoryFragment();
+                        setChecked(myHistoryFragment, rlMyHistory, rlAbout, rlMyInfo, rlMyIncome);
+                    }
+                });
+        // 我的收入
+        RxView.clicks(rlMyIncome)
+                .throttleFirst(500, TimeUnit.MILLISECONDS)
+                .compose(this.<Void>bindToLifecycle())
+                .subscribe(new Action1<Void>() {
+                    @Override
+                    public void call(Void aVoid) {
+                        MyIncomeFragment myIncomeFragment = new MyIncomeFragment();
+                        setChecked(myIncomeFragment, rlMyIncome, rlAbout, rlMyInfo, rlMyHistory);
+                    }
+                });
+        // 关于
+        RxView.clicks(rlAbout)
+                .throttleFirst(500, TimeUnit.MILLISECONDS)
+                .compose(this.<Void>bindToLifecycle())
+                .subscribe(new Action1<Void>() {
+                    @Override
+                    public void call(Void aVoid) {
+                        AboutFragment aboutFragment = new AboutFragment();
+                        setChecked(aboutFragment, rlAbout, rlMyIncome, rlMyInfo, rlMyHistory);
+                    }
+                });
     }
 
     private void init() {
 
+    }
+
+    private void setChecked(Fragment fragment, RelativeLayout llChecked, RelativeLayout ll1, RelativeLayout ll2, RelativeLayout ll3) {
+        llChecked.setBackgroundColor(getResources().getColor(R.color.colorWhite));
+        ll1.setBackgroundColor(getResources().getColor(R.color.background_divider_color));
+        ll2.setBackgroundColor(getResources().getColor(R.color.background_divider_color));
+        ll3.setBackgroundColor(getResources().getColor(R.color.background_divider_color));
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        ft.replace(R.id.ll_content, fragment);
+        ft.commit();
     }
 }
