f6e5878441f0d0ee43e6d59bb96d5b4aa0e24163
郑程鹏 <zhengchengpengisme@gmail.com>
2017-06-14 16:06:44 +0800

    增加：提现界面及界面修改

--- interhis/app/src/main/java/com/witnsoft/interhis/setting/myincome/WithdrawCrashFragment.java
+++ interhis/app/src/main/java/com/witnsoft/interhis/setting/myincome/WithdrawCrashFragment.java
@@ -2,18 +2,14 @@ package com.witnsoft.interhis.setting.myincome;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.LinearLayout;
 
 import com.jakewharton.rxbinding.view.RxView;
 import com.witnsoft.interhis.R;
 import com.witnsoft.interhis.setting.ChildBaseFragment;
-import com.witnsoft.interhis.utils.ui.ItemSettingRight;
 
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.ViewInject;
@@ -24,29 +20,15 @@ import java.util.concurrent.TimeUnit;
 import rx.functions.Action1;
 
 /**
- * Created by zhengchengpeng on 2017/6/13.
+ * Created by zhengchengpeng on 2017/6/14.
  */
 
-@ContentView(R.layout.fragment_my_income)
-public class MyIncomeFragment extends ChildBaseFragment {
+@ContentView(R.layout.fragment_withdraw_crash)
+public class WithdrawCrashFragment extends ChildBaseFragment {
     View rootView;
 
-    @ViewInject(R.id.rv_income)
-    private RecyclerView rvIncome;
-    @ViewInject(R.id.sl_refresh)
-    private SwipeRefreshLayout slRefresh;
-    @ViewInject(R.id.rl_to_cash)
-    private ItemSettingRight rlToCash;
-
-    // 账户余额
-    @ViewInject(R.id.tv_balance)
-    private TextView tvBalance;
-    // 我的总收入
-    @ViewInject(R.id.tv_income)
-    private TextView tvIncome;
-    // 本月金额
-    @ViewInject(R.id.tv_month)
-    private TextView tvMonth;
+    @ViewInject(R.id.ll_back)
+    private LinearLayout llBack;
 
     @Nullable
     @Override
@@ -61,31 +43,17 @@ public class MyIncomeFragment extends ChildBaseFragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         initClick();
-        init();
-    }
-
-    private void init() {
-        // 提现
-        rlToCash.setTvTitle(getResources().getString(R.string.to_cash));
-        tvBalance.setText("1500");
-        tvIncome.setText("3000");
-        tvMonth.setText("1000");
     }
 
     private void initClick() {
-        RxView.clicks(rlToCash)
+        RxView.clicks(llBack)
                 .throttleFirst(500, TimeUnit.MILLISECONDS)
                 .compose(this.<Void>bindToLifecycle())
                 .subscribe(new Action1<Void>() {
                     @Override
                     public void call(Void aVoid) {
-                        toWithdrawCrash();
+                        finishFragment();
                     }
                 });
-    }
-
-    private void toWithdrawCrash() {
-        WithdrawCrashFragment introductionFragment = new WithdrawCrashFragment();
-        pushFragment(introductionFragment, null, true);
     }
 }
